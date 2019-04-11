954adb9b6ba74ec6fda766fbc0e7c09437d1b1d1
Jonathan Tredway <jptredway@gmail.com>
2019-04-10 20:38:19 -0500

    Update user endpoint to handle params seperately

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -5,29 +5,30 @@ const { getUserById, addUser } = require("../../../db");
 
 const router = Router();
 
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const id = uuidv4();
+    const hash = await bcrypt.hash(password, 10);
+    const user = await addUser({ id, email, password: hash });
+    res.json(user);
+  } catch (err) {
+    console.log(`Err addding user: ${err}`);
+    res.end();
+  }
+
+  console.log(`${req.method} request at route "${req.originalUrl}"`);
+});
+
 router
-  .route("/:id?")
+  .route("/:id")
   .get(async (req, res) => {
     try {
       const user = await getUserById(req.params.id);
       res.json(user);
     } catch (err) {
       console.log(err);
-      res.end();
-    }
-
-    console.log(`${req.method} request at route "${req.originalUrl}"`);
-  })
-  .post(async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-      const id = uuidv4();
-      const hash = await bcrypt.hash(password, 10);
-      const user = await addUser({ id, email, password: hash });
-      res.json(user);
-    } catch (err) {
-      console.log(`Err addding user: ${err}`);
       res.end();
     }
 
